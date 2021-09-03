a5dc1aae044ee70b43e53d743d7dee8370e7d386
SimplyElvis <provictor.ie@gmail.com>
2021-09-03 16:05:16 +0100

    scss style test

---
+++ src/components/ExtensionCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ExtensionCard.scss";
+
+const ExtensionCard = () => {
+  return (
+    <div className="card">
+      <h1>Headng</h1>
+    </div>
+  );
+};
+
+export default ExtensionCard;
